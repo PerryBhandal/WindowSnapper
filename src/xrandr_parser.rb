07033e7bb456ef765e5800482f4daac8a7b56e3f
Perry Bhandal <perry.bhandal@gmail.com>
2014-09-21 17:03:30 -0400

    Fixed return from getMonitors to return an array of monitor hashes.

--- src/xrandr_parser.rb
+++ src/xrandr_parser.rb
@@ -14,6 +14,8 @@ class XrandrParser
         monitors.addMonitor(monitorInfo[0], monitorInfo[3].to_i, monitorInfo[1].to_i, monitorInfo[4].to_i, monitorInfo[2].to_i)
       end
     end
+
+    monitors
   end
 end
 
