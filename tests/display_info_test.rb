27bb1167d02c4badfdfb6706961a43670938d1bb
Perry Bhandal <perry.bhandal@gmail.com>
2014-09-21 14:09:19 -0400

    Renamed display info tests to automated running, rather than manual.

--- tests/display_info_test.rb
+++ tests/display_info_test.rb
@@ -1,10 +1,12 @@
 require "test/unit"
 require "mocha/test_unit"
 
+require_relative "test_helpers"
+
 class TestDisplayInfo < Test::Unit::TestCase
 
-  def testTrivial
+  def testNothing
     assert_equal(1, 1)
   end
 
-end
+end 
