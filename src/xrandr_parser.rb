30382e0933360da1be7d8ff0ffd4f263b9e96c90
Perry Bhandal <perry.bhandal@gmail.com>
2014-09-21 15:28:41 -0400

    Completed monitor parsing from Xrandr output

--- src/xrandr_parser.rb
+++ src/xrandr_parser.rb
@@ -1,3 +1,5 @@
+require_relative "./display_info.rb"
+
 class XrandrParser
 
   def initialize(xrOut)
@@ -5,6 +7,13 @@ class XrandrParser
   end
 
   def getMonitors
-    
+    monitors = Monitors.new
+    @xrOut.each do |line|
+      if line.include?(" connected ")
+        monitorInfo = line.scan(/^(.*?) connected (\d+)x(\d+)\+(\d+)\+(\d+) \(/).at(0)
+        monitors.addMonitor(monitorInfo[0], monitorInfo[3].to_i, monitorInfo[1].to_i, monitorInfo[4].to_i, monitorInfo[2].to_i)
+      end
+    end
   end
 end
+
